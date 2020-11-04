5c50cfe9f04294222e4731f9ccdd40d329b806e5
gryffinbit@gmail.com <gryffinbit@gmail.com>
2020-11-04 18:12:20 +0800

    Site updated: 2020-11-04 18:12:20

--- js/snow.js
+++ js/snow.js
@@ -12,7 +12,7 @@ $(function (){
 	var startPositionLeft = Math.random() * documentWidth - 100,
 	startOpacity = 0.5 + Math.random(),
 	sizeFlake = options.minSize + Math.random() * options.maxSize,
-	endPositionTop = documentHeight + 20000,
+	endPositionTop = documentHeight + 10000,
 	endPositionLeft = startPositionLeft - 500 + Math.random() * 500,
 	durationFall = documentHeight * 10 + Math.random() * 5000;
 	$flake.clone().appendTo('body').css({
