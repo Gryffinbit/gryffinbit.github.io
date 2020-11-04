32a82b090f4fe981e5708308ca0d88dc7de7d507
gryffinbit@gmail.com <gryffinbit@gmail.com>
2020-11-04 17:53:50 +0800

    Site updated: 2020-11-04 17:53:50

--- js/snow.js
+++ js/snow.js
@@ -9,12 +9,12 @@ $(function (){
 		flakeColor	: "#AFDAEF" /* 此处可以定义雪花颜色，若要白色可以改为#FFFFFF */
 	};
 	var interval= setInterval( function(){
-	var startPositionLeft = Math.random() * documentWidth - 100,
+	var startPositionLeft = Math.random() * documentWidth+200,
 	startOpacity = 0.5 + Math.random(),
 	sizeFlake = options.minSize + Math.random() * options.maxSize,
-	endPositionTop = documentHeight ,
-	endPositionLeft = startPositionLeft  + Math.random() * 5000,
-	durationFall = documentHeight * 100 + Math.random() * 5000;
+	endPositionTop = documentHeight + 200 ,
+	endPositionLeft = startPositionLeft  + Math.random() * 500,
+	durationFall = documentHeight * 10 + Math.random() * 5000;
 	$flake.clone().appendTo('body').css({
 		left: startPositionLeft,
 		opacity: startOpacity,
