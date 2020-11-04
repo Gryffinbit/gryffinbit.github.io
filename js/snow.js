e9d20bed17a38c4e215c14ff413430f33ef4fb28
gryffinbit@gmail.com <gryffinbit@gmail.com>
2020-11-04 17:29:32 +0800

    Site updated: 2020-11-04 17:29:31

--- js/snow.js
+++ js/snow.js
@@ -1,4 +1,3 @@
-
 $(function (){
 	var $flake = $('<div id="snowbox" />').css({'position': 'absolute','z-index':'9999', 'top': '-50px'}).html('&#10052;'),
 	documentHeight 	= $(document).height(),
@@ -10,11 +9,11 @@ $(function (){
 		flakeColor	: "#AFDAEF" /* 此处可以定义雪花颜色，若要白色可以改为#FFFFFF */
 	};
 	var interval= setInterval( function(){
-	var startPositionLeft = Math.random() * documentWidth ,
+	var startPositionLeft = Math.random() * documentWidth - 100,
 	startOpacity = 0.5 + Math.random(),
 	sizeFlake = options.minSize + Math.random() * options.maxSize,
-	endPositionTop = documentHeight ,
-	endPositionLeft = startPositionLeft  + Math.random() * 500,
+	endPositionTop = documentHeight - 200,
+	endPositionLeft = startPositionLeft - 500 + Math.random() * 500,
 	durationFall = documentHeight * 10 + Math.random() * 5000;
 	$flake.clone().appendTo('body').css({
 		left: startPositionLeft,
@@ -30,4 +29,3 @@ $(function (){
 	});
 	}, options.newOn);
 });
-
